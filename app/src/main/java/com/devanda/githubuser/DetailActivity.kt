f50e8e6735695d3c304a4a003f2145b3f570a08c
Donny Devanda <devandadonny@gmail.com>
2020-12-17 13:04:48 +0700

    DetailXML

---
+++ app/src/main/java/com/devanda/githubuser/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.devanda.githubuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
